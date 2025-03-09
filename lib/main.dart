1f6f73f8a8bae62a739d6c5724628eec8dff2a97
Arif0388 <arifhussai542@gmail.com>
2025-03-09 10:51:04 +0530

    nothing changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:news_app/Config/Theme.dart';
 import 'package:news_app/Controller/NavBottomController.dart';
 import 'package:news_app/Controller/NewsController.dart';
 import 'package:news_app/Pages/HomePage/HomePage.dart';
-import 'package:news_app/Pages/Shimmer/DemoPage.dart';
 import 'package:news_app/Pages/Shimmer/shimmer.dart';
+import 'package:news_app/Pages/Splash_Screen/Splash_Screen.dart';
 
 import 'Controller/HomePageController.dart';
 
@@ -17,19 +17,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    NewsController newsController = Get.put(NewsController());
-    BottomNavController bottomNavController = Get.put(BottomNavController());
     return GetMaterialApp(
       debugShowCheckedModeBanner:false,
       title: 'News',
       darkTheme:darkTheme,
       themeMode:ThemeMode.dark,
-      // home:const DemoPage(),
-      // // home:const LoadingContainer(width:50, height:50),
-      home:const HomepageController(),
+      home:SplashScreen()
     );
   }
 }
